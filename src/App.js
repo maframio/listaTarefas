384498b3d5897a3762c48a9ff5aa2c4cbf778fe0
arthur mafra <4rtour@gmail.com>
2022-10-13 23:51:02 -0300

    criando a interface inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BsTrash, BsBookmarkCheck, BsBookmarkCheckFill } from "react-icons";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const API = "http://localhost:5000";
+
+const App = () => {
+
+    const [titulo, setTitulo] = useState("");
+    const [tempo, setTempo] = useState("");
+    const [tarefas, setTarefas] = useState([]);
+    const [carrengando, setCarregando] = useState(false);
+
+    // let messagem = "hello vietnan world";
+    // a variavel state atualiza o componente
+    return (
+        <div className="app">
+            <div className="cabecalho-tarefas">
+                <h1>lista de tarefas</h1>
+            </div>
+
+            <div className="formulario">
+                <p>Formulário</p>
+            </div>
+
+            <div className="lista">
+                <p>Listas</p>
+            </div>
+        </div>
+
+
+    );
+};
 
 export default App;
